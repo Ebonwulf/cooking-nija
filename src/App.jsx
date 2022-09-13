718fc8bec7316c7da0376f2a56a919c72f7dcbdc
ebonwulf <ebonwolf00@gmail.com>
2022-09-13 13:40:41 +0100

    routing added for all the pages in the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Create from './pages/Create/Create';
+import Search from './pages/Search/Search';
+import Recipe from './pages/Recipe/Recipe';
 import './App.scss';
 
 function App() {
-  return <div className='App'>Hi</div>;
+  return (
+    <div className='App'>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route path='/create'>
+            <Create />
+          </Route>
+          <Route path='/recipes/:id'>
+            <Recipe />
+          </Route>
+          <Route path='/search'>
+            <Search />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
