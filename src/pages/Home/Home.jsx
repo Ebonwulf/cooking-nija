3bd181313e1152a1aba2f491b0f1598516a35e4a
ebonwulf <ebonwolf00@gmail.com>
2022-09-14 09:14:05 +0100

    recipeList component created and logic and styles completed to display the recipe details on the screen. useFetch hook implemented in the home component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,17 @@
+import RecipeList from '../../components/RecipeList/RecipeList';
+import { useFetch } from '../../hooks/useFetch';
 import './Home.scss';
 
 const Home = () => {
-  return <div>Home</div>;
+  const { data, isPending, error } = useFetch('http://localhost:3000/recipes');
+
+  return (
+    <div className='home'>
+      {error && <p className='error'>{error}</p>}
+      {isPending && <p className='loading'>Loading...</p>}
+      {data && <RecipeList recipes={data} />}
+    </div>
+  );
 };
 
 export default Home;
