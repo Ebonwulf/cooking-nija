ecbd015405925d700cc54a0891fe72b93cd32269
ebonwulf <ebonwolf00@gmail.com>
2022-09-14 10:33:33 +0100

    styling added for the create page component and most of the form completed in the create.jsx

--- src/pages/Create/Create.jsx
+++ src/pages/Create/Create.jsx
@@ -1,7 +1,51 @@
+import { useState } from 'react';
 import './Create.scss';
 
 const Create = () => {
-  return <div>Create</div>;
+  const [title, setTitle] = useState('');
+  const [method, setMethod] = useState('');
+  const [cookingTime, setCookingTime] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(title, method, cookingTime);
+  };
+
+  return (
+    <div className='create'>
+      <h2 className='page-title'>Add a new recipe</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Recipe title: </span>
+          <input
+            type='text'
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+            required
+          ></input>
+        </label>
+        <label>
+          <span>Recipe method: </span>
+          <textarea
+            onChange={(e) => setMethod(e.target.value)}
+            value={method}
+            required
+          />
+        </label>
+        {/*ingredients will go here*/}
+        <label>
+          <span>Cooking time (minutes) </span>
+          <input
+            type='number'
+            onChange={(e) => setCookingTime(e.target.value)}
+            value={cookingTime}
+            required
+          ></input>
+        </label>
+        <button className='btn'>Submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default Create;
